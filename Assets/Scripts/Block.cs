335bee2cbbcb477b792ea71e879192e5a170b854
xiaotu1234 <157554741+xiaotu1234@users.noreply.github.com>
2024-12-01 17:13:13 +0800

    传！

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -8,6 +8,7 @@ public class Block : MonoBehaviour
     public Animator Animator;
     public Animator Nianshou;
     public GameObject Jiesuan;
+    public Animator Score;
     // Start is called before the first frame update
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -20,6 +21,7 @@ public class Block : MonoBehaviour
                 Nianshou.SetTrigger("Fail");
                 Animator.SetBool("Zhengyan",true);
                 Jiesuan.SetActive(true);
+                Score.SetTrigger("Jiesuan");
             }
             else
             {
--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -7,14 +7,15 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour
 {
     private float score = 0;
-
     [SerializeField]private Text textScore;
 
     public void AddScore(float value){
         score += value;
+
     }
 
     private void Update(){
-        textScore.text = "" + score;
+        int scoree = Mathf.FloorToInt(score);
+        textScore.text = "" + scoree;
     }
 }
