2ccf475585f747d1cb4380f2655d23ab00955f0e
xiaotu1234 <157554741+xiaotu1234@users.noreply.github.com>
2024-12-01 15:02:07 +0800

    改了game场景

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -6,6 +6,7 @@ public class Block : MonoBehaviour
 {
     public GameObject background;
     public Animator Animator;
+    public Animator Nianshou;
     // Start is called before the first frame update
     void OnCollisionEnter2D(Collision2D collision)
     {
@@ -15,6 +16,7 @@ public class Block : MonoBehaviour
             // 触发动画
             if (background != null)
             {
+                Nianshou.SetTrigger("Fail");
                 Animator.SetBool("Zhengyan",true);  // 替换为你动画的触发器名称
             }
             else
