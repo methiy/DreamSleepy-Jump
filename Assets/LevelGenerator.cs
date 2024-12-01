79ad10f7448ecaebe4031bca484c6e2eeaaa7895
zzh <3250949283@qq.com>
2024-12-01 11:26:47 +0800

    Enemy Prefab

--- Assets/LevelGenerator.cs
+++ Assets/LevelGenerator.cs
@@ -6,14 +6,16 @@ public class LevelGenerator : MonoBehaviour
 {
     public GameObject[] platformPrefabs;
     public GameObject[] propsPrefabs;
-
     public GameObject[] bulletPrefabs;
+    public GameObject[] enemyPrefabs;
+
     float currentYPos = 0f;
     public float cameraHeight = 5.5f;
 
     public Transform platformPool;
     public Transform propsPool;
     public Transform bulletPool;
+    public Transform enemyPool;
 
     [SerializeField]private const float BOOMSIZE = 0.35f;
 
@@ -23,18 +25,20 @@ public class LevelGenerator : MonoBehaviour
         SpawnPlatformPool();
         SpawnPropsPool();
         SpawnBulletPool();
+        SpawnEnemyPool();
 
         while(currentYPos < Camera.main.transform.position.y + cameraHeight)
         {
             PickNewPlatform();
+            if(Random.Range(0,100) <= 10)  CreateNewEnemy();
         }
     }
 
-    void SpawnPlatformPool()
+    private void SpawnPlatformPool()
     {
-        int basicPlatformAmount = 30;
-        int weakPlatformAmount = 15;
-        int movePlatformAmount = 10;
+        int basicPlatformAmount = 20;
+        int weakPlatformAmount = 10;
+        int movePlatformAmount = 5;
 
         for (int i = 0; i < basicPlatformAmount; i++)
         {
@@ -63,7 +67,7 @@ public class LevelGenerator : MonoBehaviour
             GameObject platform = Instantiate(propsPrefabs[0], propsPool);
             platform.SetActive(false);
         }
-        int rocketPropsAmount = 5;
+        int rocketPropsAmount = 1;
         for (int i = 0; i < rocketPropsAmount; i++)
         {
             GameObject platform = Instantiate(propsPrefabs[1], propsPool);
@@ -88,7 +92,7 @@ public class LevelGenerator : MonoBehaviour
         return null;;
     }
 
-    void PickNewPlatform()
+    private void PickNewPlatform()
     {
         currentYPos += Random.Range(0.6f, 1f);
         float xPos = Random.Range(-3.5f, 3.5f);
@@ -102,10 +106,13 @@ public class LevelGenerator : MonoBehaviour
         platformPool.GetChild(r).position = new Vector2(xPos, currentYPos);
         platformPool.GetChild(r).gameObject.SetActive(true);
 
-        //create props
-        if(Random.Range(0,100) <= 10){
-            CreateNewProps(xPos,currentYPos + BOOMSIZE);
+        if(platformPool.GetChild(r).gameObject.GetComponent<Platform>().platformType == PlatformType.normal){
+            //create props
+            if(Random.Range(0,100) <= 10){
+                CreateNewProps(xPos,currentYPos + BOOMSIZE);
+            }
         }
+        
     }
 
     private void CreateNewProps(float xPos,float yPos){
@@ -121,6 +128,36 @@ public class LevelGenerator : MonoBehaviour
         if(chanceCount >=10 )   return ;
         propsPool.GetChild(r).position = new Vector2(xPos, yPos);
         propsPool.GetChild(r).gameObject.SetActive(true);
+
+    }
+
+    private void SpawnEnemyPool(){
+        int movingEnemyAmount = 5;
+        for (int i = 0; i < movingEnemyAmount; i++)
+        {
+            GameObject enemy = Instantiate(enemyPrefabs[0], enemyPool);
+            enemy.SetActive(false);
+        }
+        int shadowEnemyAmount = 1;
+        for (int i = 0; i < shadowEnemyAmount; i++)
+        {
+            GameObject enemy = Instantiate(enemyPrefabs[1], enemyPool);
+            enemy.SetActive(false);
+        }
+    }
+    private void CreateNewEnemy(){
+
+        currentYPos += Random.Range(0.6f, 1f);
+        float xPos = Random.Range(-2f, 2f);
+
+        int r = 0;
+        do
+        {
+            r = Random.Range(0, enemyPool.childCount);
+        } while (enemyPool.GetChild(r).gameObject.activeInHierarchy);
+
+        enemyPool.GetChild(r).position = new Vector2(xPos, currentYPos);
+        enemyPool.GetChild(r).gameObject.SetActive(true);
 
     }
     // Update is called once per frame
--- Assets/Scripts/Enemy/ShadowEnemy.cs
+++ Assets/Scripts/Enemy/ShadowEnemy.cs
@@ -6,52 +6,14 @@ using UnityEngine.UI;
 public class ShadowEnemy : BaseEnemy
 {
     public float fadeSpeed = 5f; // 增加速度以适应1秒的转换
-    private Image image;
+    [SerializeField]private Image image;
     private bool isFading = false; // 控制是否正在执行淡入淡出
-
-    void Awake()
-    {
-        image = GetComponent<Image>();
-    }
 
     void Start()
     {
-        StartCoroutine(FadeCycle());
+        image = Camera.main.GetComponent<Image>();
     }
 
-    // 开始淡入淡出循环
-    IEnumerator FadeCycle()
-    {
-        while (true)
-        {
-            yield return StartCoroutine(FadeToColor(Color.black, 0.5f)); // 0.5秒从白色到黑色
-            yield return StartCoroutine(FadeToColor(Color.white, 0.5f)); // 0.5秒从当前颜色到白色
-        }
-    }
-
-    // 淡入到指定的颜色
-    IEnumerator FadeToColor(Color targetColor, float duration)
-    {
-        if (isFading) yield break; // 如果已经在淡入淡出了，就不再开始新的
-        isFading = true;
-
-        float timeElapsed = 0;
-        Color startColor = image.color;
-
-        while (timeElapsed < duration)
-        {
-            image.color = Color.Lerp(startColor, targetColor, timeElapsed / duration);
-            timeElapsed += Time.deltaTime;
-            yield return null;
-        }
-
-        image.color = targetColor; // 确保最终达到目标颜色
-        isFading = false;
-    }
-
-    void OnDestroy()
-    {
-        StopAllCoroutines(); // 清理所有协程
-    }
+    
 
 }
